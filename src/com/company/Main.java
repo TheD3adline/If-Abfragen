d8612e45bee46dbf1a931c9ff5acb1d69596d2dc
MichaelFessler-Desktop <michaelfessler@gmx.at>
2022-01-08 17:19:25 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int age = 15;
+        age = age + 1;
+
+        if(age > 17) {
+            System.out.println("Dann darfst Du den Schnaps kaufen!");
+        }
+        else {
+            System.out.println("Du darfst den Schnaps leider nicht kaufen, da Du zu jung bist! Der ist ab 18!!!");
+        }
+    }
+}
